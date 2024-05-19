f64fe533560c17453d012c1db830c814b2a8a731
yasnosolnce2024 <pomogaika51@gmail.com>
2024-05-19 08:50:16 +0700

    add code

--- lightsource/lightsource/spiders/lightsoursenew.py
+++ lightsource/lightsource/spiders/lightsoursenew.py
@@ -1,10 +1,14 @@
 import scrapy
 
-
 class LightsoursenewSpider(scrapy.Spider):
     name = "lightsoursenew"
     allowed_domains = ["https://divan.ru"]
     start_urls = ["https://www.divan.ru/kemerovo/category/svet"]
 
     def parse(self, response):
-        pass
+        lihts = response.css('_Ud0k')
+        for light in lihts:
+            yield {'name':light.css('div.lsooF span::text').get(),
+                   'price': light.css('div.pY3d2 span::text').get(),
+                   'url': light.css('a').attrib['href']
+                   }
